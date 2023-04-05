21af65e26492c0c08e2266c9c3f4cc292989bb72
Oguzhan Yilmaz <oguzhan@kloia.com>
2023-04-05 23:56:14 +0300

    adding message to custom_nodes __init__.py file

--- balcony/custom_nodes/__init__.py
+++ balcony/custom_nodes/__init__.py
@@ -1,13 +1,22 @@
 """
-When a Python module is imported as `from <module> import *`,
-modules `__init__.py` file is imported.  
+All custom classes must be exported here. 
+
+balcony dynamically imports custom_nodes folder like this:
+```python
+import custom_nodes
+```
+
+When only package name is given, python will try to import the __init__.py file in the package.
+So, all custom classes must be exported here to be imported by balcony.
+
+Cheers.
 """
 
 from .ec2 import *
 from .codebuild import *
 from .ecs import *
 from .lambda_functions import *
-from .iam import *
 from .ssm import *
-from .s3 import *
 from .ses import *
+from .iam import *
+from .s3 import *
