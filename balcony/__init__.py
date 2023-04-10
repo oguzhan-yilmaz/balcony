1875da98b9ac36d56b84cd072e3e4ea2b6c86d5c
Oguzhan Yilmaz <oguzhan@kloia.com>
2023-04-10 23:38:26 +0300

    trying to fix parent package import problem

--- balcony/__init__.py
+++ balcony/__init__.py
@@ -3,15 +3,21 @@ Any resource that app system is going to use must be exported here.
 Otherwise different import statements would create new global static objects
 for AppRegistry etc.
 """
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 try:
-    from .aws import BalconyAWS, Boto3SessionSingleton
+    from .aws import BalconyAWS
     from .config import get_rich_console, get_logger
     from .nodes import ServiceNode, ResourceNode
     from .reader import ServiceReader
     from .relations import RelationMap
     from .errors import Error
 except ImportError:
-    from balcony.aws import BalconyAWS, Boto3SessionSingleton
+    # print('ImportError: balcony/__init__.py')
+    from aws import BalconyAWS
     from config import get_rich_console, get_logger
     from nodes import ServiceNode, ResourceNode
     from reader import ServiceReader
