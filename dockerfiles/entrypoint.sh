e28fae8d8953cf31cf469c6a7e0529bc7b242d52
Oguzhan Yilmaz <oguzhan@kloia.com>
2023-06-25 17:57:02 +0300

    initializing terraform provider

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -35,16 +35,54 @@ pip3 show balcony
 
 echo "Using $GEN_TF_DIR directory to save generated terraform files."
 
-echo "Using Terraform AWS provider with profile: $AWS_PROFILE and region: $AWS_DEFAULT_REGION"
-batcat -P -p  << EOF >> $GEN_TF_DIR/provider.tf
+
+
+echo "Generating 'provider \"aws\" {}' block in $GEN_TF_DIR/provider.tf file"
+
+if [[ -n "${AWS_PROFILE}" ]]; then
+  echo "The AWS_PROFILE environment variable is set to '${AWS_PROFILE}'."
+  cat << EOF >> $GEN_TF_DIR/provider.tf
 
 provider "aws" {
-    profile = "$AWS_PROFILE"
-    region = "$AWS_DEFAULT_REGION"
+    profile   = "$AWS_PROFILE"
+    region    = "$AWS_DEFAULT_REGION"
 }
 EOF
+
+
+elif [[ -n "${AWS_SESSION_TOKEN}" ]]; then
+  # when using a assumed role, there's AWS_SESSION_TOKEN associated with it. Support that.
+  echo "The AWS_SESSION_TOKEN environment variable is set to '${AWS_SESSION_TOKEN}'."
+  cat << EOF >> $GEN_TF_DIR/provider.tf
+
+provider "aws" {
+    access_key  = "$AWS_ACCESS_KEY_ID"
+    secret_key  = "$AWS_SECRET_ACCESS_KEY"
+    token       = "$AWS_SESSION_TOKEN"
+    region      = "$AWS_DEFAULT_REGION"
+}
+EOF
+
+elif [[ -n "${AWS_ACCESS_KEY_ID}" ]]; then
+  echo "The AWS_ACCESS_KEY_ID environment variable is set to '${AWS_ACCESS_KEY_ID}'."
+  cat << EOF >> $GEN_TF_DIR/provider.tf
+
+provider "aws" {
+    access_key  = "$AWS_ACCESS_KEY_ID"
+    secret_key  = "$AWS_SECRET_ACCESS_KEY"
+    region      = "$AWS_DEFAULT_REGION"
+}
+EOF
+
+else
+  echo "Neither the AWS_PROFILE nor the AWS_ACCESS_KEY_ID environment variable is set. Please set one of them and rerun the script."
+  exit 1
+fi
+
+
+
 echo "--------------------------"
-batcat -P -p   $GEN_TF_DIR/provider.tf
+batcat -P  $GEN_TF_DIR/provider.tf
 echo "--------------------------"
 
 
@@ -55,7 +93,7 @@ python3 balcony/cli.py terraform-import "$@" -o $GEN_TF_DIR/generated_imports.tf
 
 echo "Balcony has generated the following import blocks:"
 echo "--------------------------"
-batcat -P -p  $GEN_TF_DIR/generated_imports.tf
+batcat -P $GEN_TF_DIR/generated_imports.tf
 echo "--------------------------"
 
 
@@ -68,6 +106,6 @@ echo "You may see stderr output of terraform above this. It is expected, as the 
 
 echo "Terraform has finished generating the terraform code"
 echo "--------------------------"
-batcat -P -p  tf_generated.tf
+batcat -P tf_generated.tf
 echo "--------------------------"
 exit 0
--- dockerfiles/terraform-import.Dockerfile
+++ dockerfiles/terraform-import.Dockerfile
@@ -9,16 +9,17 @@ WORKDIR $GEN_TF_DIR
 COPY dockerfiles/provider.tf $GEN_TF_DIR/provider.tf
 
 # copy over the entrypoint script
-COPY  --chmod=0755 dockerfiles/entrypoint.sh entrypoint.sh
+COPY --chmod=0755 dockerfiles/entrypoint.sh entrypoint.sh
 
 RUN apt update -y \
-    && apt install -y curl unzip bat -y \ 
+    && apt install -y curl unzip bat \ 
     && apt autoremove -y \
     && curl -q https://releases.hashicorp.com/terraform/1.5.1/terraform_1.5.1_linux_amd64.zip -o terraform.zip \
     && unzip -q terraform.zip \
     && mv terraform /usr/local/bin/ \
     && rm -rf terraform.zip \
     && pip3 install --no-cache-dir balcony \
+    && terraform init -upgrade \
     && chmod +x entrypoint.sh
 
 ENTRYPOINT [ "./entrypoint.sh"]
