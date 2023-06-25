a8db77db78b8a42d82d5a51551aca44b2b6924b0
Oguzhan Yilmaz <oguzhan@kloia.com>
2023-06-25 18:24:13 +0300

    updating the entrypoint.sh with debug_echo

--- dockerfiles/entrypoint.sh
+++ dockerfiles/entrypoint.sh
@@ -1,43 +1,41 @@
 #!/bin/bash
 
-# info logs the given argument at info log level.
-info() {
-    echo "[INFO] " "$@"
+function debug_echo {
+  if [[ $BALCONY_DEBUG -eq 1 ]]; then
+    if [[ -p /dev/stdin ]]; then
+      while IFS= read -r line
+      do
+        echo "$line"
+      done
+    else
+      echo "$@"
+    fi
+  fi
 }
 
-# warn logs the given argument at warn log level.
-warn() {
-    echo "[WARN] " "$@" >&2
-}
-
-# fatal logs the given argument at fatal log level.
-fatal() {
-    echo "[ERROR] " "$@" >&2
-    exit 1
-}
-
-echo "Docker entrypoint script started to run"
+debug_echo "Docker entrypoint script started to run"
 
 
-terraform version
+debug_echo "Printing the terraform version"
+terraform version | debug_echo
+debug_echo ""
+
 tf_version_output=$(terraform version)
 
 
 # Check if the output contains the desired string
 if [[ $tf_version_output == *"Your version of Terraform is out of date!"* ]]; then
-    echo "TODO: Upgrade the terraform!" # TODO: maybe exit with 1?
-# else
-#     echo "The output does not contain 'Your version of Terraform is out of date!'"
+    echo "TODO: Upgrade the terraform to new version!" # TODO: maybe exit with 1?
 fi
 
+debug_echo "Printing the balcony version"
+pip3 show balcony | debug_echo
+balcony info | debug_echo
 
-pip3 show balcony
-
-echo "Using $GEN_TF_DIR directory to save generated terraform files."
+debug_echo "Using $GEN_TF_DIR directory to save generated terraform files."
 
 
-
-echo "Generating 'provider \"aws\" {}' block in $GEN_TF_DIR/provider.tf file"
+debug_echo "Generating 'provider \"aws\" {}' block in $GEN_TF_DIR/provider.tf file"
 
 if [[ -n "${AWS_PROFILE}" ]]; then
   echo "The AWS_PROFILE environment variable is set to '${AWS_PROFILE}'."
@@ -48,7 +46,6 @@ provider "aws" {
     region    = "$AWS_DEFAULT_REGION"
 }
 EOF
-
 
 elif [[ -n "${AWS_SESSION_TOKEN}" ]]; then
   # when using a assumed role, there's AWS_SESSION_TOKEN associated with it. Support that.
@@ -82,16 +79,16 @@ fi
 
 
 echo "--------------------------"
-batcat   $GEN_TF_DIR/provider.tf
+batcat  $GEN_TF_DIR/provider.tf
 echo "--------------------------"
 
 
 
-echo "Running balcony terraform-import command to generate import blocks"
+debug_echo "Running balcony terraform-import command to generate import blocks"
 
-python3 balcony/cli.py terraform-import "$@" -o $GEN_TF_DIR/generated_imports.tf
+balcony terraform-import "$@" -o $GEN_TF_DIR/generated_imports.tf
 
-echo "Balcony has generated the following import blocks:"
+debug_echo "Balcony has generated the following import blocks:"
 echo "--------------------------"
 batcat  $GEN_TF_DIR/generated_imports.tf
 echo "--------------------------"
@@ -99,13 +96,16 @@ echo "--------------------------"
 
 
 pushd $GEN_TF_DIR/
-echo "Generating terraform files for import blocks using terraform plan -generate-config-out= command."
+debug_echo "Generating terraform files for import blocks using terraform plan -generate-config-out= command."
 terraform plan -generate-config-out=tf_generated.tf
 
+echo "--------------------------"
 echo "You may see stderr output of terraform above this. It is expected, as the import feature under active development."
 
-echo "Terraform has finished generating the terraform code"
+debug_echo "Terraform has finished generating the terraform code"
 echo "--------------------------"
 batcat  tf_generated.tf
 echo "--------------------------"
+debug_echo "Script has finished. Exiting successfully."
+
 exit 0
